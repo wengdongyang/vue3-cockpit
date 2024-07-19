a335e938001f3c42741cfee4ccc52e07111c6139
wdy <wdy@pro.com>
2024-07-19 15:29:59 +0800

    feat：更新依赖文件

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,3 @@
 /** @format */
 
 /// <reference types="vite/client" />
-
-declare module 'qs';
-declare module 'nprogress';
-declare module '*.vue' {
-  import { Component } from 'vue';
-  const component: Component;
-  export default component;
-}
